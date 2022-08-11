144da0e63f180c95e1f470615c4d483da894b1a1
Pablo Tato Ramos <pabtatoramos@gmail.com>
2022-08-11 17:40:56 +0200

    Add init function for Rectangle

--- raylib.zig
+++ raylib.zig
@@ -46,6 +46,9 @@ pub const Rectangle = extern struct {
     y: f32,
     width: f32,
     height: f32,
+    pub fn init(x: f32, y: f32, width: f32, height: f32) Rectangle {
+        return Rectangle{ .x = x, .y = y, .width = width, .height = height };
+    }
 };
 pub const Image = extern struct {
     data: ?*anyopaque,

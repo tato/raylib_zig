419006780e404d06cd3e985871eb3982fa6278ec
Pablo Tato Ramos <pabtatoramos@gmail.com>
2022-10-30 19:58:48 +0100

    Add "BeginScissorModeRec" helper

--- raylib.zig
+++ raylib.zig
@@ -630,6 +630,14 @@ pub extern fn EndShaderMode() void;
 pub extern fn BeginBlendMode(mode: c_int) void;
 pub extern fn EndBlendMode() void;
 pub extern fn BeginScissorMode(x: c_int, y: c_int, width: c_int, height: c_int) void;
+pub fn BeginScissorModeRec(rect: Rectangle) void {
+    BeginScissorMode(
+        @floatToInt(c_int, rect.x),
+        @floatToInt(c_int, rect.y),
+        @floatToInt(c_int, rect.width),
+        @floatToInt(c_int, rect.height),
+    );
+}
 pub extern fn EndScissorMode() void;
 pub extern fn BeginVrStereoMode(config: VrStereoConfig) void;
 pub extern fn EndVrStereoMode() void;
